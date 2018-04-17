e175c17574a01dd77d75f09c2b035fe49df929f7
hoyinho <hoyinho999@gmail.com>
2018-04-17 17:29:46 -0400

    added modularity

--- database/dbQuery.js
+++ database/dbQuery.js
@@ -10,18 +10,14 @@ db.on("error", console.error.bind(console, "connection error:"));
 
 db.once("open", function(){
     console.log("we are connected!");
-    get_System_Field("Hoyin", '6500228', "nodes.cpuAvgMax").then(function(field){
-	console.log(field);
-    });
 });
-
 function get_All_Systems(username){
     return user.findOne({'username': username}).select("systems");
 }
 
 function get_All_Alerts(username){
     return user.findOne({'username': username}).select("alerts");
-};
+}
 
 function create_Alert(username, name, threshold, field){
     const newAlert = {
@@ -50,3 +46,4 @@ function get_System_Field(username, systemSerial, field){
     return user.findOne({"username": username, "systems.serialnumberInserv": systemSerial}).select("systems." + field);
 }
 
+module.exports = {get_All_Systems, get_All_Alerts, create_Alert, change_Alert, add_User} 
