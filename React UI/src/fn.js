4c90695de62e0dc8ab0a07f13a0dd1b85eca8fab
unknown <elisesymmes@gmail.com>
2018-05-01 12:53:30 -0400

    Code cleanup: removed unused variables from state, replaced == with === when applicable, changed state.value to state.sysSelect to better describe what it actually is, changed handleChange to handleSysSelect to better describe what it actually is.

--- React UI/src/fn.js
+++ React UI/src/fn.js
@@ -4,16 +4,16 @@ import none from './imgs/noneSelected.png'
 import React, { Component } from 'react';
 
 export function triggered(arrTrig, index){
-  if(index=="0"){
+  if(index===0){
 	  return <img src={none} height="20%" width="40%" alt="Nothing is happening"/>;
   }
-  if (arrTrig.length=="0") {
+  if (arrTrig.length===0) {
 	 return <img src={good} height="20%" width="40%" alt="Nothing it happening, or only good things are happening"/>;
   }
 	return <img src={bad} height="20%" width="40%" alt="Things are happening that might not be good"/>;
 }
 export function listAlerts(list, bullet){
-	if(list.length=="0"){
+	if(list.length===0){
 		return <ul><li key={ 0 }> {<b><img src={bullet} height="16" width="16" alt="fancy bullet points"/>{ "\xa0\xa0\xa0\xa0No alerts to display"}</b>}</li></ul>;
 	}
 	var alertList = list.map(function(name, index){
@@ -23,24 +23,30 @@ export function listAlerts(list, bullet){
 }
 
 export function listStatusesLeft(list, bullet){
-	if(list.length=="0"){
+	if(list.length===0){
 		return <ul><li key={ 0 }> {<b><img src={bullet} height="16" width="16" alt="fancy bullet points"/>{ "No statuses to display"}</b>}</li></ul>;
 	}
 	var alertList = list.map(function(name, index){
 		if(index>0){
 			return <li key={ index }> {<b><img src={bullet} height="16" width="16" alt="fancy bullet points"/>{ "\xa0" + name[0]}</b>}</li>;
 		}
+		else{
+			return "";	
+		} 
 	})
 	return <ul>{alertList}</ul>;
 }
 export function listStatusesRight(list, bullet){
-	if(list.length=="0"){
+	if(list.length===0){
 		return <ul><li key={ 0 }> {<b><img src={bullet} height="16" width="16" alt="fancy bullet points"/>{ "\xa0\xa0\xa0\xa0No alerts to display"}</b>}</li></ul>;
 	}
 	var alertList = list.map(function(name, index){
 		if(index>0){
 			return <li key={ index }> {<b>{ "\xa0\xa0\xa0\xa0"+ name[1]+"\xa0\xa0\xa0"}</b>}</li>;
 		}
+		else{
+			return "";	
+		} 
 	})
 	return <ul>{alertList}</ul>;
 }
